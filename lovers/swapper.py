9046c89bfde601207e1e52e4d87e033884455119
XunylYasna <lynux_ansay@dlsu.edu.ph>
2020-08-22 16:43:25 +0800

    accepted swapper

--- lovers/swapper.py
+++ lovers/swapper.py
@@ -1,57 +1,105 @@
 import math
 
-def bubbleSortAscending(arr):
-    count = 0
-    n = len(arr) 
+def mergea(arr, temp, left, mid, right): 
+    inv_count = 0
   
-    # Traverse through all array elements 
-    for i in range(n): 
+    i = left #i is index for left subarray*/ 
+    j = mid #i is index for right subarray*/ 
+    k = left #i is index for resultant merged subarray*/ 
+    while ((i <= mid - 1) and (j <= right)): 
+        if (arr[i] <= arr[j]): 
+            temp[k] = arr[i] 
+            k += 1
+            i += 1
+        else: 
+            temp[k] = arr[j] 
+            k += 1
+            j += 1
   
-        # Last i elements are already in place 
-        for j in range(0, n-i-1): 
-  
-            # traverse the array from 0 to n-i-1 
-            # Swap if the element found is greater 
-            # than the next element 
-            if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
-                count += 1
+            inv_count = inv_count + (mid - i) 
 
-    return count
+    while (i <= mid - 1): 
+        temp[k] = arr[i] 
+        k += 1
+        i += 1
 
-def bubbleSortDescending(arr):
-    count = 0
-    n = len(arr) 
-    # print(arr,"fd")
-    # Traverse through all array elements 
-    for i in range(n): 
+    while (j <= right): 
+        temp[k] = arr[j] 
+        k += 1
+        j += 1
   
-        # Last i elements are already in place 
-        for j in range(0, n-i-1): 
+    for i in range(left,right+1,1): 
+        arr[i] = temp[i] 
   
-            # traverse the array from 0 to n-i-1 
-            # Swap if the element found is less than 
-            # than the next element 
-            if arr[j] < arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
-                count += 1
-                # print(arr)
+    return inv_count 
 
-    return count
+def merged(arr, temp, left, mid, right): 
+    inv_count = 0
+  
+    i = left #i is index for left subarray*/ 
+    j = mid #i is index for right subarray*/ 
+    k = left #i is index for resultant merged subarray*/ 
+    while ((i <= mid - 1) and (j <= right)): 
+        if (arr[i] >= arr[j]): 
+            temp[k] = arr[i] 
+            k += 1
+            i += 1
+        else: 
+            temp[k] = arr[j] 
+            k += 1
+            j += 1
+  
+            inv_count = inv_count + (mid - i) 
+
+    while (i <= mid - 1): 
+        temp[k] = arr[i] 
+        k += 1
+        i += 1
+
+    while (j <= right): 
+        temp[k] = arr[j] 
+        k += 1
+        j += 1
+  
+    for i in range(left,right+1,1): 
+        arr[i] = temp[i] 
+  
+    return inv_count 
+  
+
+def _mergeSort(arr, temp, left, right, ad): 
+    inv_count = 0
+    
+    if (right > left): 
+        mid = int((right + left)/2) 
+
+        inv_count = _mergeSort(arr, temp, left, mid, ad) 
+        inv_count += _mergeSort(arr, temp, mid+1, right, ad) 
+
+        if(ad == 1):
+            inv_count += mergea(arr, temp, left, mid+1, right)
+        else:
+            inv_count += merged(arr, temp, left, mid+1, right)
+  
+    return inv_count 
 
 
 def solve(books,n,s):
     # TODO: Compute and print answer here
     books1 = books.copy()
+    temp = [0 for i in range(n)] 
+    temp1 = [0 for i in range(n)] 
 
-    sa = bubbleSortAscending(books)
-    sd = bubbleSortDescending(books1)
-
+    sa = _mergeSort(books, temp, 0, n - 1,1) 
+    sd = _mergeSort(books1, temp1, 0, n - 1,0) 
+    # print(sa,sd, s)
     swaps = min(sa,sd)
     
+    if swaps == 0:
+        return "Butz loses!"
     
     ans = s/swaps + 1
-    # print(sa,sd,swaps, s, ans)
+    
     #return quantum blasts per second (int)
     return math.floor(ans)
 def main():
