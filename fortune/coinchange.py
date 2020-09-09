ce1ccf069bade9e2749b576bfe89243565507724
russhenson <49965783+russhenson@users.noreply.github.com>
2020-09-09 21:41:16 +0800

    updated

--- fortune/coinchange.py
+++ fortune/coinchange.py
@@ -1,22 +1,15 @@
-def solve(d,denoms,v):
-    # compute and return answer here
-    MAX = int(1e5)
-
-    dp = [0] * (v+1)
-    dp[0] = 1
-
-    for denom in denoms:
-        for i in range(1, len(dp)):
-            if denom <= i:
-                dp[i] += dp[i-denom]
-    
-    return dp[v]
-
-
 d, q = list(map(int,input().rstrip().split(" ")))
 denoms = sorted(list(map(int,input().rstrip().split(" "))))
 
-# preprocess here
+MAX = int(3e5) + 1
+
+dp = [0] * MAX
+dp[0] = 1
+
+for denom in denoms:
+    for i in range(1, MAX):
+        if denom <= i:
+            dp[i] += dp[i-denom]
 
 ans_list = []
 for i in range(q):
@@ -24,5 +17,5 @@ for i in range(q):
     
     # build answer to avoid TLE
     # you can redefine the solve function as needed
-    ans_list.append(solve(d,denoms,v))
+    ans_list.append(dp[v])
 print("{}".format("\n".join(list(map(str,ans_list)))))
