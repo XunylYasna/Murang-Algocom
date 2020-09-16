324b823e10e5dc78a123b15c8d6e2f143eda34c7
Russ Henson <henson.russ10@gmail.com>
2020-09-16 17:27:27 +0800

    strengthprobset

---
+++ strength/cudoviste.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int r, c;
+    cin >> r >> c;
+    vector<vector<char>> v;
+    for(int i = 0; i < r; i++) {
+        vector<char> temp;
+        for(int j = 0; j < c; j++) {
+            char character;
+            cin >> character;
+            temp.push_back(character);
+        }
+        v.push_back(temp);
+    }
+
+    vector<int> ans = {0, 0, 0, 0, 0};
+
+    for(int i = 0; i < r-1; i++) {
+        for(int j = 0; j < c-1; j++) {
+            int cars = 0;
+
+            if(v[i][j] == 'X') {
+                cars++;
+            }
+            if(v[i+1][j] == 'X') {
+                cars++;
+            }
+            if(v[i][j+1] == 'X') {
+                cars++;
+            }
+            if(v[i+1][j+1] == 'X') {
+                cars++;
+            }
+
+            if(v[i][j] == '#') {
+                continue;
+            }
+            if(v[i+1][j] == '#') {
+                continue;
+            }
+            if(v[i][j+1] == '#') {
+                continue;
+            }
+            if(v[i+1][j+1] == '#') {
+                continue;
+            }
+
+            ans[cars]++;
+        }
+    }
+
+    for(auto i : ans) {
+        cout << i << endl;
+    }
+}
