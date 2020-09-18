3359e64daa2e992a6ede5cbd275ab245f94e72ed
Miguel Landrito <miggi941@gmail.com>
2020-09-19 01:24:43 +0800

    tangina mo nicolas

---
+++ hanged/miggi code/teleportingchecker.py
@@ -0,0 +1,18 @@
+MOD = int(1e9) + 7
+a, n, Q = list(map(int,input().strip().split(" ")))
+
+MAX = int(2e5) + 1
+dp = [0] * MAX
+
+for i in range(MAX):
+    if i < n:
+        dp[i] = a
+    else:
+        dp[i] = (dp[i-n] + dp[i-n+1]) % MOD
+
+outs = []
+for i in range(Q):
+    qi = int(input())
+    outs.append(dp[qi])
+    
+print("\n".join(list(map(str,outs))))
